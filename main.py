689b0c95eefea9b51b319eab4d36336a91306d44
Mykhailo Holub <mishataube2005@gmail.com>
2025-05-22 21:38:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from logging import WARNING
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
@@ -25,10 +27,28 @@ from pycocotools.coco import COCO
 import numpy as np
 
 import dataset_loader
+import warnings
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-
+print("Loading MS COCO dataset.")
 coco_train = dataset_loader.coco_load_train()
-coco_test, coco_train = keras.utils.split_dataset(coco_train, left_size=0.8)
 coco_val = dataset_loader.coco_load_val()
+print("MS COCO loaded. Splitting it into train and test.")
+
+def is_test(x, y):
+    return x % 4 == 0
+
+def is_train(x, y):
+    return not is_test(x, y)
+
+recover = lambda x,y: y
+
+coco_test = coco_train.enumerate() \
+                    .filter(is_test) \
+                    .map(recover)
+
+coco_train = coco_train.enumerate() \
+                    .filter(is_train) \
+                    .map(recover)
+print("MS COCO splitted and ready for work.")
