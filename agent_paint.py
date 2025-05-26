ef4a6f3ca882c2fa19de7c195ccf8eca58922b33
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-05-26 18:14:02 +0300

    Some design and try paint

--- agent_paint.py
+++ agent_paint.py
@@ -1,7 +1,8 @@
 ## IMPORTS
 import dataset_loader as dl
 import datasets_from_loader_utils as dflu
-from some_functions import *
+import some_functions as sf
+import os
 
 import numpy as np
 
@@ -16,27 +17,41 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 ## PARAMETERS
 dl.IMAGE_SIZE = 128
-dl.BATCH_SIZE = 32
-EPOCHS = 2
+dl.BATCH_SIZE = 64
+EPOCHS = 3
 
-MODEL_PATH = "models/max128_5_12.keras"
+AGENT1_MODELS = "models/Agent1/"
+# AGENT1_MODEL_NAME = "max128_5_12.keras"
+AGENT1_MODEL_NAME = "max128_5_12.keras"
+custom_objects = {
+    'weighted_combined_loss': sf.weighted_combined_loss,
+    # 'weighted_sparse_categorical_crossentropy': sf.weighted_sparse_categorical_crossentropy,
+    'WeightedMeanIoU': sf.WeightedMeanIoU(num_classes=dl.COCO_NUM_CLASSES)
+}
+
+AGENT2_MODELS = "models/Agent2/"
+count = len(os.listdir(AGENT2_MODELS))
+
+MODEL_PATH = AGENT1_MODELS + AGENT1_MODEL_NAME
+# MODEL_SAVE_PATH = AGENT2_MODELS + "test{count}".format(count=count) + "_e{epoch:02d}_l{val_loss:.4f}_m{val_mae:.4f}.keras"
+MODEL_SAVE_PATH = AGENT2_MODELS + "test{count}".format(count=count) + "_e{epoch:02d}_l{val_loss:.4f}.keras"
+MODEL_LOAD_PATH = AGENT2_MODELS + "test32_e02_l0.1288.keras"
+# print(MODEL_PATH)
+# print(MODEL_SAVE_PATH)
 
 TFRECORD_PATH_TRAIN = "tfrecords/Agent2_train.tfrecord"  # 128x128 train precomputed images and masks
 # TFRECORD_PATH_TRAIN = "tfrecords/train_32.tfrecord"  # 32x32 train precomputed images and masks
 TFRECORD_PATH_VAL = "tfrecords/Agent2_val.tfrecord"  # 128x128 test precomputed images and masks
 # TFRECORD_PATH_VAL = "tfrecords/val_32.tfrecord"  # 32x32 test precomputed images and masks
 
-TRAIN_STEPS, VAL_STEPS = dl.coco_steps()
-DATASET_RATIO = 100  # 1/?
-TRAIN_STEPS, VAL_STEPS = TRAIN_STEPS//DATASET_RATIO, VAL_STEPS//DATASET_RATIO
+# TRAIN_STEPS, VAL_STEPS = dl.coco_steps()
+# DATASET_RATIO = 400  # 1/?
+TRAIN_STEPS, VAL_STEPS = 10, 10
 
 ## LOAD MODEL FROM AGENT 1
 msh_model = tf.keras.models.load_model(
     MODEL_PATH,
-    custom_objects={
-        'weighted_combined_loss': weighted_combined_loss,
-        'WeightedMeanIoU': WeightedMeanIoU(num_classes=dl.COCO_NUM_CLASSES)
-    }
+    custom_objects=custom_objects
 )
 
 ## LOAD TRAIN AND VAL DATASETS TODO: ADD RGB IMAGES IF THIS IS NECESSARY
@@ -62,61 +77,96 @@ data_augmentation = tf.keras.Sequential([
 ])
 
 # TODO: RECEIVE GRAYSCALE IMAGE, PREDICT MASK USING AGENT 1 MODEL, MAKE RGB IMAGE USING AGENT 2 MODEL TAKING GRAYSCALE IMAGE AND MASK AS BASE, BUT GENERETE RGB IMAGE BY ITSELF
+"""
 msh_model.trainable = False  # optional
 
 # Input to the final model
 input_gray = layers.Input(shape=(dl.IMAGE_SIZE, dl.IMAGE_SIZE, 1))  # (?, 128, 128, 1)
 
 # Internal call to Agent 1
-agent1_mask = msh_model(input_gray)  # (?, 128, 128, 9)
-agent1_mask = tf.argmax(agent1_mask, axis=3)[..., tf.newaxis]  # (?, 128, 128, 1)
-d3_gray_image = tf.concat([input_gray for _ in range(3)], axis=3)
-coco_rgb_colors = tf.constant([
-    [255, 255, 255],  # light
-    [0, 0, 0],        # dark
-    [255, 0, 0],      # red
-    [0, 255, 0],      # green
-    [0, 0, 255],      # blue
-    [0, 255, 255],    # cyan
-    [255, 255, 0],    # yellow
-    [255, 0, 255],    # magenta
-    [128, 128, 128],  # gray
-])
-agent1_mask = tf.squeeze(agent1_mask, axis=3)
-agent1_mask = tf.gather(coco_rgb_colors, agent1_mask)
-agent1_mask = d3_gray_image * (tf.cast(agent1_mask, tf.float32) / 255)
+segmentation_output = msh_model(input_gray)  # (?, 128, 128, 9)
+main_segmentation_output = tf.argmax(segmentation_output, axis=3)  # (?, 128, 128)
+d3_gray_image = tf.concat([input_gray for _ in range(3)], axis=3)  # (?, 128, 128, 3)
+
+# agent1_mask = tf.squeeze(agent1_mask, axis=3)  # (?, 128, 128)
+agent1_rgb_mask = tf.gather(coco_rgb_colors, main_segmentation_output)  # (?, 128, 128, 3)
+agent1_rgb_mask = (tf.cast(agent1_rgb_mask, tf.float32) / 255)  # (?, 128, 128, 3)
+agent1_image_with_mask = d3_gray_image * agent1_rgb_mask  # (?, 128, 128, 3)
+agent1_image_with_mask = tf.image.rgb_to_hsv(agent1_image_with_mask)
 
 # Concatenate input and Agent 1 output
-x = layers.Concatenate(axis=-1)([input_gray, agent1_mask])  # (?, 128, 128, 4)
+x = layers.Concatenate()([input_gray, agent1_image_with_mask])  # (None, 128, 128, 4)
 
-x = layers.Conv2D(dl.IMAGE_SIZE, (5, 5), activation='relu', padding='same')(x)
+x = layers.Conv2D(64, (3, 3), padding='same')(x)
 x = layers.BatchNormalization()(x)
-x = layers.MaxPooling2D(2, 2)(x)
+x = layers.Activation('relu')(x)
 
-x = layers.Conv2D(dl.IMAGE_SIZE, (5, 5), activation='relu', padding='same')(x)
+x = layers.Conv2D(128, (3, 3), padding='same')(x)
 x = layers.BatchNormalization()(x)
-x = layers.MaxPooling2D(4, 4)(x)
+x = layers.Activation('relu')(x)
 
-x = layers.Dropout(0.3)(x)
-
-x = layers.UpSampling2D(4)(x)
-x = layers.Conv2DTranspose(dl.IMAGE_SIZE, (5, 5), activation='relu', padding='same')(x)
+x = layers.Conv2D(64, (3, 3), padding='same')(x)
 x = layers.BatchNormalization()(x)
+x = layers.Activation('relu')(x)
 
-x = layers.UpSampling2D(2)(x)
-x = layers.Conv2DTranspose(dl.IMAGE_SIZE // 2, (5, 5), activation='relu', padding='same')(x)
-x = layers.BatchNormalization()(x)
-
-x = layers.Conv2DTranspose(dl.IMAGE_SIZE // 4, (5, 5), activation='relu', padding='same')(x)
-x = layers.BatchNormalization()(x)
-
-x = layers.Conv2DTranspose(dl.IMAGE_SIZE // 8, (5, 5), activation='relu', padding='same')(x)
-x = layers.BatchNormalization()(x)
-
-output = layers.Conv2D(3, 1, activation='softmax')(x)
-
+hs = layers.Conv2D(2, (1, 1), activation='sigmoid')(x)  # Hue and Saturation (None, 128, 128, 2)
+output = layers.Concatenate(axis=3)([hs, input_gray])  # (None, 128, 128, 3)
+# output = layers.Lambda(lambda x: tf.image.hsv_to_rgb(x))(hsv)  # (None, 128, 128, 3)
 model = tf.keras.Model(inputs=input_gray, outputs=output)
-"""model = tf.keras.models.Sequential(
+"""
+
+input_gray = layers.Input(shape=(dl.IMAGE_SIZE, dl.IMAGE_SIZE, 1))  # (None, 128, 128, 1)
+input_mask = layers.Input(shape=(dl.IMAGE_SIZE, dl.IMAGE_SIZE, 3))  # (None, 128, 128, 3)
+
+x = layers.Concatenate(axis=-1)([input_gray, input_mask])  # (None, 128, 128, 4)
+
+x = layers.Conv2D(64, (3, 3), padding='same')(x)
+x = layers.BatchNormalization()(x)
+x = layers.Activation('relu')(x)
+
+x = layers.Conv2D(128, (3, 3), padding='same')(x)
+x = layers.BatchNormalization()(x)
+x = layers.Activation('relu')(x)
+
+x = layers.Conv2D(64, (3, 3), padding='same')(x)
+x = layers.BatchNormalization()(x)
+x = layers.Activation('relu')(x)
+
+hs = layers.Conv2D(2, (1, 1), activation='sigmoid')(x)  # Hue and Saturation (None, 128, 128, 2)
+output = layers.Concatenate(axis=3)([hs, input_gray])  # (None, 128, 128, 3)
+model = tf.keras.Model(inputs=[input_gray, input_mask], outputs=output)
+
+
+# x = layers.Conv2D(dl.IMAGE_SIZE, (5, 5), activation='relu', padding='same')(x)
+# x = layers.BatchNormalization()(x)
+# x = layers.MaxPooling2D(2, 2)(x)
+
+# x = layers.Conv2D(dl.IMAGE_SIZE, (5, 5), activation='relu', padding='same')(x)
+# x = layers.BatchNormalization()(x)
+# x = layers.MaxPooling2D(4, 4)(x)
+
+# x = layers.Dropout(0.3)(x)
+
+# x = layers.UpSampling2D(4)(x)
+# x = layers.Conv2DTranspose(dl.IMAGE_SIZE, (5, 5), activation='relu', padding='same')(x)
+# x = layers.BatchNormalization()(x)
+
+# x = layers.UpSampling2D(2)(x)
+# # x = layers.Conv2D(dl.IMAGE_SIZE // 2, (5, 5), activation='relu', padding='same')(x)
+# # x = layers.BatchNormalization()(x)
+
+# # x = layers.Conv2D(dl.IMAGE_SIZE // 4, (5, 5), activation='relu', padding='same')(x)
+# # x = layers.BatchNormalization()(x)
+
+# # x = layers.Conv2D(dl.IMAGE_SIZE // 8, (5, 5), activation='relu', padding='same')(x)
+# # x = layers.BatchNormalization()(x)
+
+# output = layers.Conv2D(3, 1, activation='softmax')(x)
+
+# model = tf.keras.Model(inputs=input_gray, outputs=output)
+
+"""
+model = tf.keras.models.Sequential(
     [
         layers.Input(shape=(dl.IMAGE_SIZE, dl.IMAGE_SIZE, 1)),  # RECEIVE (?, 128, 128, 1), values from 0 to 1 or 0 to 255
         # TODO: ADD MASK FROM AGENT 1
@@ -156,15 +206,40 @@ model = tf.keras.Model(inputs=input_gray, outputs=output)
         layers.Conv2D(3, 1, activation='softmax')  # RETURN (?, 128, 128, 3), values from 0 to 1 or 0 to 255
     ]
 )
-"""
 model.summary()
+"""
+
+
+# def ImageQuality(y_true, y_pred):
+#     return tf.image.psnr(y_true, y_pred, max_val=1.0)
+
+
+# def PerceptualSimilarity(y_true, y_pred):
+#     return tf.image.ssim(y_true, y_pred, max_val=1.0)
+
+
+# def combined_loss(y_true, y_pred):
+#     mse = tf.reduce_mean(tf.square(y_true - y_pred))
+#     ssim = 1 - tf.reduce_mean(PerceptualSimilarity(y_true, y_pred))
+#     psnr = tf.reduce_mean(1/(ImageQuality(y_true, y_pred) + 1e-10))
+#     return 0.5 * mse + 0.2 * ssim + 0.3 * psnr
+
 
 # TODO: VALIDATE MODEL
+# """
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-    loss=tf.keras.losses.MeanSquaredError(),
-    metrics=['mae', 'accuracy']
+    # loss='mse',
+    loss=sf.combined_loss_agent2,
+    # metrics=[
+    #     'mae',
+    #     ImageQuality,
+    #     PerceptualSimilarity,
+    # ]
+    # loss=tf.keras.losses.MeanSquaredError(),
+    # metrics=['mae', 'accuracy']
 )
+# """
 
 ## TRAIN MODEL
 # """
@@ -175,8 +250,10 @@ callbacks = [
         restore_best_weights=True
     ),
     tf.keras.callbacks.ModelCheckpoint(
-        MODEL_PATH.split(".")[0] + "__{val_loss:.2f}.keras",
-        monitor='val_loss', save_best_only=True
+        MODEL_SAVE_PATH,
+        monitor='val_loss',
+        save_best_only=True,
+        save_freq='epoch'
     )
 ]
 history = model.fit(
@@ -188,10 +265,13 @@ history = model.fit(
     callbacks=callbacks
 )
 # """
-# model = tf.keras.models.load_model(MODEL_PATH.split(".")[0] + "_0.09.keras")
+model = tf.keras.models.load_model(
+    MODEL_LOAD_PATH,
+    custom_objects={"combined_loss": combined_loss}
+)
 
 ## DISPLAY HISTORY
-# """
+"""
 def display_history(history, names, title):
     plt.figure()
     plt.title(title)
@@ -204,9 +284,10 @@ def display_history(history, names, title):
 
 
 display_history(history, ['loss', 'val_loss'], "Loss")
-display_history(history, ['accuracy', 'val_accuracy'], "Accuracy")
+# display_history(history, ['accuracy', 'val_accuracy'], "Accuracy")
 display_history(history, ['mae', 'val_mae'], "MeanAbsoluteError")
-# """
+display_history(history, ['ImageQuality', 'val_ImageQuality'], "ImageQuality")
+"""
 
 
 ## Check model performance
@@ -224,12 +305,6 @@ images.insert(0, "plane.jpg")
 images.insert(0, "apple.jpg")
 for file_name in images:
     try:
-        # file_name = "datasets\\kode-lgx-dr-ghostx.jpg"
-
-        # for images, mask in coco_train.take(1):
-        #     print("train images batch shape:", images.shape)
-        #     print("train masks  batch shape:", mask.shape)
-
         image_ = cv2.imread("datasets\\test2017\\" + file_name, cv2.IMREAD_COLOR_RGB)
         image_ = cv2.resize(image_, (dl.IMAGE_SIZE, dl.IMAGE_SIZE))
 
@@ -250,6 +325,7 @@ for file_name in images:
     image = np.expand_dims(image, axis=0)/255
 
     rgb_pred = model.predict(image)
+    rgb_pred = tf.image.hsv_to_rgb(rgb_pred)
 
     plt.subplot(1, 2, 2)
     plt.title("Predicted RGB")
